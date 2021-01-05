604a9a37ab232a11326897bc5439f3b9f2ff9862
talysh <taleh.aliyev@gmail.com>
2021-01-05 15:08:02 -0500

    implemented table sort and filter functionality on hard coded data

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,128 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import * as ReactBootStrap from "react-bootstrap";
+import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import employees from "./utils/employees";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h2>Employee Directory w/React!</h2>
+
+class DefaultSortTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.options = {
+      defaultSortName: 'firstName',  // default sort column name
+      defaultSortOrder: 'desc', // default sort order
+    };
+  }
+  render() {
+    
+    const selectRow = {
+      mode: 'checkbox',  // multi select
+      clickToSelect: true,
+      bgColor: 'lightBlue'
+    };
+
+    const cellEdit = {
+      mode: 'click',
+      blurToSave: true // click cell to edit
+    };
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Employee Directory w/React!</h2>
+        </div>
+        <BootstrapTable data={employees} options={this.options} selectRow={selectRow} cellEdit={cellEdit} hover>
+          <TableHeaderColumn dataField='firstName' dataSort filter={ { type: 'TextFilter', delay: 500 } }>First Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='lastName' dataSort>Last Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='role' dataSort>Role</TableHeaderColumn>
+          <TableHeaderColumn dataField='telephone' isKey dataSort>Telephone</TableHeaderColumn>
+          <TableHeaderColumn dataField='email' dataSort>Email</TableHeaderColumn>
+          <TableHeaderColumn dataField='address'>Address</TableHeaderColumn>
+          <TableHeaderColumn dataField='tenure' dataSort>Tenure</TableHeaderColumn>
+        </BootstrapTable>
       </div>
-      {/* <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p> */}
-    </div>
-  );
+    );
+  }
 }
 
+// const App = () => {
 
-export default App;
+//   const employees = [
+//     {
+//       firstName: "Taleh",
+//       lastName: "Aliyev",
+//       role: "manager",
+//       tenure: "5",
+//       telephone: "781-444-3345",
+//       email: "taleh@aliyev.com",
+//       address: "208 Beaver St. Framingham, MA"
+//     },
+//     {
+//       firstName: "Elias",
+//       lastName: "Aliyev",
+//       role: "engineer",
+//       tenure: "2",
+//       telephone: "508-888-9037",
+//       email: "elias@aliyev.com",
+//       address: "23 Kingsbury St. Framingham, MA"
+//     }, {
+//       firstName: "Eralda",
+//       lastName: "Aliyeva",
+//       role: "executive",
+//       tenure: "3",
+//       telephone: "508-960-2027",
+//       email: "eralda@aliyev.com",
+//       address: "185 Fulton St. Medford, MA"
+//     }
+//   ];
+
+//   const renderEmployees = (employee, index) => {
+//     return (
+//       <tr key="{index}">
+//         <td>{employee.firstName}</td>
+//         <td>{employee.lastName}</td>
+//         <td>{employee.role}</td>
+//         <td>{employee.telephone}</td>
+//         <td>{employee.email}</td>
+//         <td>{employee.address}</td>
+//         <td>{employee.tenure}</td>
+//       </tr>
+//     )
+
+//   }
+
+//   return (
+
+//     <div className="App">
+//       <div className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         <h2>Employee Directory w/React!</h2>
+//       </div>
+//       <p className="App-intro">
+//         {/* To get started, edit <code>src/App.js</code> and save to reload. */}
+//       </p>
+
+//       <ReactBootStrap.Table striped bordered hover>
+//         <thead>
+//           <tr>
+//             <th>First Name</th>
+//             <th>Last Name</th>
+//             <th>Role</th>
+//             <th>Telephone</th>
+//             <th>Email</th>
+//             <th>Address</th>
+//             <th>Tenure</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {employees.map(renderEmployees)}
+//         </tbody>
+//       </ReactBootStrap.Table>
+//     </div>
+//   );
+// }
+
+export default DefaultSortTable;
+// export default App;
---
+++ client/src/utils/employees.js
@@ -0,0 +1,53 @@
+const employees = [
+    {
+      firstName: "Tony",
+      lastName: "Smith",
+      role: "manager",
+      tenure: "5",
+      telephone: "781-434-3345",
+      email: "tony@smith.com",
+      address: "697 Random St. Quincy, MA"
+    }, {
+      firstName: "Rigers",
+      lastName: "Aliyev",
+      role: "engineer",
+      tenure: "2",
+      telephone: "403-828-5035",
+      email: "rigers@aliyev.com",
+      address: "232 Temple St. Framingham, MA"
+    }, {
+      firstName: "Oleg",
+      lastName: "Dmitry",
+      role: "executive",
+      tenure: "3",
+      telephone: "508-360-2027",
+      email: "oleg@dmitry.com",
+      address: "185 Fulton St. Medford, MA"
+    }, {
+      firstName: "Rambo",
+      lastName: "Stallone",
+      role: "manager",
+      tenure: "7",
+      telephone: "781-544-3435",
+      email: "rambo@stallone.com",
+      address: "2208 James Bond St. New York, MA"
+    }, {
+      firstName: "Jack",
+      lastName: "Darius",
+      role: "engineer",
+      tenure: "1",
+      telephone: "957-737-8100",
+      email: "jack@darius.com",
+      address: "1123 Hollywood St. Boca Raton, MA"
+    }, {
+      firstName: "Arben",
+      lastName: "Rushiti",
+      role: "executive",
+      tenure: "3",
+      telephone: "774-963-2823",
+      email: "arben@rushiti.com",
+      address: "185 Fulton St. Medford, MA"
+    }
+  ];
+
+  export default employees;
