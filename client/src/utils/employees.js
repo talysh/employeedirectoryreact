f741b7c37aa2c9bdbe27c69c56169b677a0bef68
talysh <taleh.aliyev@gmail.com>
2021-01-05 20:19:46 -0500

    moved employee seeds into a separate file, cleaned up code, added functionality to filter on name, address, and tenure

--- client/src/utils/employees.js
+++ client/src/utils/employees.js
@@ -1,53 +1,102 @@
 const employees = [
     {
-      firstName: "Tony",
-      lastName: "Smith",
-      role: "manager",
-      tenure: "5",
-      telephone: "781-434-3345",
-      email: "tony@smith.com",
-      address: "697 Random St. Quincy, MA"
+        firstName: "Tony",
+        lastName: "Smith",
+        role: "manager",
+        tenure: "5",
+        telephone: "781-434-3345",
+        email: "tony@smith.com",
+        address: "697 Random St. Quincy, MA"
     }, {
-      firstName: "Rigers",
-      lastName: "Aliyev",
-      role: "engineer",
-      tenure: "2",
-      telephone: "403-828-5035",
-      email: "rigers@aliyev.com",
-      address: "232 Temple St. Framingham, MA"
+        firstName: "Rigers",
+        lastName: "Kikino",
+        role: "engineer",
+        tenure: "2",
+        telephone: "403-828-5035",
+        email: "rigers@kikino.com",
+        address: "232 Temple St. Framingham, MA"
     }, {
-      firstName: "Oleg",
-      lastName: "Dmitry",
-      role: "executive",
-      tenure: "3",
-      telephone: "508-360-2027",
-      email: "oleg@dmitry.com",
-      address: "185 Fulton St. Medford, MA"
+        firstName: "Oleg",
+        lastName: "Dmitry",
+        role: "executive",
+        tenure: "3",
+        telephone: "508-360-2027",
+        email: "oleg@dmitry.com",
+        address: "185 Fulton St. Medford, MA"
     }, {
-      firstName: "Rambo",
-      lastName: "Stallone",
-      role: "manager",
-      tenure: "7",
-      telephone: "781-544-3435",
-      email: "rambo@stallone.com",
-      address: "2208 James Bond St. New York, MA"
+        firstName: "Rambo",
+        lastName: "Stallone",
+        role: "manager",
+        tenure: "7",
+        telephone: "781-544-3435",
+        email: "rambo@stallone.com",
+        address: "2208 James Bond St. Milton, MA"
     }, {
-      firstName: "Jack",
-      lastName: "Darius",
-      role: "engineer",
-      tenure: "1",
-      telephone: "957-737-8100",
-      email: "jack@darius.com",
-      address: "1123 Hollywood St. Boca Raton, MA"
+        firstName: "Matthew",
+        lastName: "Darius",
+        role: "engineer",
+        tenure: "1",
+        telephone: "957-737-8100",
+        email: "jack@darius.com",
+        address: "1123 Hollywood St. Revere, MA"
     }, {
-      firstName: "Arben",
-      lastName: "Rushiti",
-      role: "executive",
-      tenure: "3",
-      telephone: "774-963-2823",
-      email: "arben@rushiti.com",
-      address: "185 Fulton St. Medford, MA"
+        firstName: "Arben",
+        lastName: "Rushiti",
+        role: "executive",
+        tenure: "3",
+        telephone: "774-963-2823",
+        email: "arben@rushiti.com",
+        address: "851 Melrose St. Melrose, MA"
+    }, {
+        firstName: "Jack",
+        lastName: "Daniels",
+        role: "marketing",
+        tenure: "5",
+        telephone: "763-101-9001",
+        email: "jack@daniels.com",
+        address: "99 Brooks St. Malden, MA"
+    }, {
+        firstName: "Ayden",
+        lastName: "David",
+        role: "CEO",
+        tenure: "5",
+        telephone: "507-834-9541",
+        email: "ayden@david.com",
+        address: "100 Kings Rd. Newton, MA"
+    }, {
+        firstName: "Oliver",
+        lastName: "Cunnings",
+        role: "designer",
+        tenure: "4",
+        telephone: "501-335-2308",
+        email: "jayden@cunnings.com",
+        address: "1 Lost way. Worcester, MA"
+    }, {
+        firstName: "Edward",
+        lastName: "Musk",
+        role: "janitor",
+        tenure: "10",
+        telephone: "430-029-5784",
+        email: "edward@musk.com",
+        address: "3 Tesla Ct. Edgartown, MA"
+    }, {
+        firstName: "Jacob",
+        lastName: "Goodwin",
+        role: "attorney",
+        tenure: "5",
+        telephone: "781-841-5867",
+        email: "jacob@goodwin.com",
+        address: "776 Tired Rd. Cambridge, MA"
+    }, {
+        firstName: "Adam",
+        lastName: "DePalma",
+        role: "finance",
+        tenure: "1",
+        telephone: "857-303-7795",
+        email: "adam@depalma.com",
+        address: "231 Fulton St. Boston, MA"
     }
-  ];
 
-  export default employees;
+];
+
+export default employees;
