a716087a0d914868f113e3bf97df70a29a63a7ca
talysh <taleh.aliyev@gmail.com>
2021-01-05 20:39:19 -0500

    edited readme file

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# employeedirectoryreact
+# Employee Directory
+    
+  ## Desription
+          
+  Employee directory build with react
+  
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+  npm i, npm start
+  
+  ## Usage
+  Allow users to check basic information about employees
+  
+  ## License
+  ![License](https://img.shields.io/badge/License-MIT-blue.svg "License Badge")
+  
+  ## Contributing
+  talysh
+  
+  ## Tests
+  none
+  
+  ## Questions
+  
+  For more information:
+  Visit: [https://talysh.github.io](https://talysh.github.io)
+  Email: taleh.aliyev@gmail.com
+  
+   ## Deployed app link
+
+  [https://pacific-ravine-21360.herokuapp.com/](https://pacific-ravine-21360.herokuapp.com/)
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
@@ -17,8 +16,6 @@ class DefaultSortTable extends React.Component {
   }
   render() {
 
-
-
     const selectRow = {
       mode: 'checkbox',  // multi select
       clickToSelect: true,
@@ -27,7 +24,7 @@ class DefaultSortTable extends React.Component {
 
     const cellEdit = { // click cell to edit
       mode: 'click',
-      blurToSave: true 
+      blurToSave: true
     };
 
     return (
@@ -36,15 +33,15 @@ class DefaultSortTable extends React.Component {
           <h2>Employee Directory w/React!</h2>
         </div>
         <BootstrapTable data={employees} options={this.options} selectRow={selectRow} cellEdit={cellEdit} hover>
-          <TableHeaderColumn dataField='firstName' dataSort filter={ { type: 'TextFilter', delay: 500 } }>First Name</TableHeaderColumn>
+          <TableHeaderColumn dataField='firstName' dataSort filter={{ type: 'TextFilter', delay: 500 }}>First Name</TableHeaderColumn>
           <TableHeaderColumn dataField='lastName' dataSort>Last Name</TableHeaderColumn>
           <TableHeaderColumn dataField='role' dataSort>Role</TableHeaderColumn>
           <TableHeaderColumn dataField='telephone' isKey>Telephone</TableHeaderColumn>
           <TableHeaderColumn dataField='email'>Email</TableHeaderColumn>
-          <TableHeaderColumn dataField='address' filter={ { type: 'TextFilter', delay: 500 } }>Address</TableHeaderColumn>
-          <TableHeaderColumn dataField='tenure' filter={ { type: 'TextFilter', delay: 500 } }>Tenure</TableHeaderColumn>
+          <TableHeaderColumn dataField='address' filter={{ type: 'TextFilter', delay: 500 }}>Address</TableHeaderColumn>
+          <TableHeaderColumn dataField='tenure' filter={{ type: 'TextFilter', delay: 500 }}>Tenure</TableHeaderColumn>
         </BootstrapTable>
-      <p className="hint">Click on FirstName, LastName, and Role categories to sort by those fields.</p>
+        <p className="hint">Click on FirstName, LastName, and Role categories to sort by those fields.</p>
       </div>
     );
   }
